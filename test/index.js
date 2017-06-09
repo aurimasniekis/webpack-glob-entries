f3d6858c493807168320fc46958c7439a44ff37e
Shawn Rapp <srapp@shutterfly.com>
2017-06-09 08:16:24 -0700

    Added an optional suffix parameter.

--- test/index.js
+++ test/index.js
@@ -20,4 +20,16 @@ describe('webpack_glob_entires', function() {
 
         assert.deepEqual(webpack_glob_entries(globFixture), { foo: '/foo/foo.js', bar: '/bar/bar.js' });
     });
+
+    it('returns entries hash with suffixed keys', function() {
+        var globFixture = '/**/*.js';
+        glob_stub.sync = function(glob) {
+            glob.should.equal(globFixture);
+
+            return ['/foo/foo.js', '/bar/bar.js'];
+        };
+
+        assert.deepEqual(webpack_glob_entries(globFixture, '_test'), { foo_test: '/foo/foo.js', bar_test: '/bar/bar.js' });
+        
+    });
 });
